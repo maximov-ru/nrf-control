463c079d8368ed6995ae24d867b64e1294dd1af4
maximov <maximov-ru>
2016-03-10 01:21:16 +0300

    test

--- remote.js
+++ remote.js
@@ -6,30 +6,33 @@ var NRF24 = require("nrf"),
 var pipes = [0xF1F0F0F0E1, 0xF1F0F0F0D2];
 var radiocfg = {spiDev:"/dev/spidev0.0", cePin:25};
 var radio = NRF24.connect(radiocfg.spiDev, radiocfg.cePin);
+try {
+    radio.dataRate('1Mbps').crcBytes(2);
+    radio.transmitPower('PA_MAX').autoRetransmit({count:15, delay:4000});
+    radio.begin(function (e) {
+        var rx = radio.openPipe('rx', pipes[1]),
+            tx = radio.openPipe('tx', pipes[0]);
+        tx.on('ready', function () {
+            var buf = new Uint32Array(1);
+            buf[0] = 81;
+            var ret = 'hz';
+            try {
+                ret = tx.write(new Buffer(buf));
+            }catch(e) {
+               console.log('=((');
+            }
+            console.log(ret);
+            //tx.write("Hello?");
+            //tx.write("blah blah blah");
+            //tx.write("the number 4");
+            //setInterval(tx.write.bind(tx, "beep"), 2e3);
+            //setInterval(tx.write.bind(tx, "boop"), 2e3);
+        });
 
-radio.dataRate('1Mbps').crcBytes(2);
-radio.transmitPower('PA_MAX').autoRetransmit({count:15, delay:4000});
-radio.begin(function (e) {
-    var rx = radio.openPipe('rx', pipes[1]),
-        tx = radio.openPipe('tx', pipes[0]);
-    tx.on('ready', function () {
-        var buf = new Uint32Array(1);
-        buf[0] = 81;
-        var ret = 'hz';
-        try {
-            ret = tx.write(new Buffer(buf));
-        }catch(e) {
-           console.log('=((');
-        }
-        console.log(ret);
-        //tx.write("Hello?");
-        //tx.write("blah blah blah");
-        //tx.write("the number 4");
-        //setInterval(tx.write.bind(tx, "beep"), 2e3);
-        //setInterval(tx.write.bind(tx, "boop"), 2e3);
+        rx.on('data', function (d) {
+            console.log("Got data:", d.toString());
+        });
     });
-
-    rx.on('data', function (d) {
-        console.log("Got data:", d.toString());
-    });
-});
+}catch(e) {
+    console.log('hm....');
+}
