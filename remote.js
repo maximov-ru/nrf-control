d13d7d07565c5c9e9ad94285d2b85ad3a65b849b
maximov <maximov-ru>
2016-03-11 00:18:28 +0300

    test

--- remote.js
+++ remote.js
@@ -7,14 +7,15 @@ var pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2];
 var radiocfg = {spiDev:"/dev/spidev0.0", cePin:25};
 var radio = NRF24.connect(radiocfg.spiDev, radiocfg.cePin);
 try {
-    radio._debug = true;
+    radio._debug = false;
     radio.dataRate('1Mbps').crcBytes(2);
     radio.transmitPower('PA_MAX').autoRetransmit({count:5, delay:4000});
     radio.setStates({DYNPD:0x00, FEATURE:0x00,EN_DPL:0});
 
     radio.begin(function (e) {
-        var rx = radio.openPipe('rx', pipes[0],{size: 'auto'}),
+        var rx = radio.openPipe('rx', pipes[0],{size: 32}),
             tx = radio.openPipe('tx', pipes[1],{autoAck:false});
+        radio.setStates({DYNPD:0x00, FEATURE:0x00,EN_DPL:0});
         tx.on('ready', function () {
             radio.printDetails();
             var buf = new Uint32Array(1);
