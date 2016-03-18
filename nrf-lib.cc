6e8e1688dcc441f433d1f3748239dc031bd39026
maximov <maximov-ru>
2016-03-19 02:26:38 +0300

    test

--- nrf-lib.cc
+++ nrf-lib.cc
@@ -2,16 +2,29 @@
 #include <node_buffer.h>
 #include <v8.h>
 
-using namespace v8;
-void init(Handle<Object> target) {
-  target->Set(String::NewSymbol("create"), FunctionTemplate::New(create)->GetFunction());
-}
-NODE_MODULE(nrf-lib, init)
+namespace demo {
 
-Handle<Value> create(const Arguments& args) {
- HandleScope scope;
- Handle<Object> ret = Object::New();
+using v8::Function;
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Null;
+using v8::Object;
+using v8::String;
+using v8::Value;
 
- node::Buffer *buf;
- return scope.Close(ret);
+void RunCallback(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  Local<Function> cb = Local<Function>::Cast(args[0]);
+  const unsigned argc = 1;
+  Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "hello world") };
+  cb->Call(Null(isolate), argc, argv);
 }
+
+void Init(Local<Object> exports, Local<Object> module) {
+  NODE_SET_METHOD(module, "exports", RunCallback);
+}
+
+NODE_MODULE(addon, Init)
+
+}  // namespace demo
