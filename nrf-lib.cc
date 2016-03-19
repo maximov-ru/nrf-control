9afee5ccb1021569a4be837ab18b104b89f0a397
maximov <maximov-ru>
2016-03-19 03:50:26 +0300

    test

--- nrf-lib.cc
+++ nrf-lib.cc
@@ -1,11 +1,15 @@
 #include <node_buffer.h>
 #include <node.h>
 #include <v8.h>
+#include <RF24.h>
 
 using namespace v8;
 
+
+
 Handle<Value> Method(const Arguments& args) {
   HandleScope scope;
+  RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
   return scope.Close(String::New("world"));
 }
 
