53dba8cb0c3ec5476b5a35ffb79434c757f01632
maximov <maximov-ru>
2016-03-19 03:06:03 +0300

    test

--- nrf-lib.cc
+++ nrf-lib.cc
@@ -1,30 +1,17 @@
-#include <node.h>
 #include <node_buffer.h>
+#include <node.h>
 #include <v8.h>
 
-namespace demo {
+using namespace v8;
 
-using v8::Function;
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Null;
-using v8::Object;
-using v8::String;
-using v8::Value;
-
-void RunCallback(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  Local<Function> cb = Local<Function>::Cast(args[0]);
-  const unsigned argc = 1;
-  Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "hello world") };
-  cb->Call(Null(isolate), argc, argv);
+Handle<Value> Method(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(String::New("world"));
 }
 
-void Init(Local<Object> exports, Local<Object> module) {
-  NODE_SET_METHOD(module, "exports", RunCallback);
+void Init(Handle<Object> exports) {
+  exports->Set(String::NewSymbol("hello"),
+      FunctionTemplate::New(Method)->GetFunction());
 }
 
-NODE_MODULE(addon, Init)
-
-}  // namespace demo
+NODE_MODULE(hello, Init)
